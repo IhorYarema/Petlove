3b3830f8c4d7c61b307a3152849e944c29b91b8c
Ihor <u96ok69@gmail.com>
2026-01-10 14:32:09 +0200

    fix PetBlock: make recktangle

--- src/components/PetBlock/PetBlock.jsx
+++ src/components/PetBlock/PetBlock.jsx
@@ -1,6 +1,7 @@
 import css from "./PetBlock.module.css";
 import cat1x from "../../assets/catregister.png";
 import cat2x from "../../assets/catregister@2x.png";
+import Icon from "../Icon/Icon";
 
 export default function PetBlock() {
   return (
@@ -11,6 +12,7 @@ export default function PetBlock() {
         srcSet={`${cat1x} 1x, ${cat2x} 2x`}
         className={css.img}
       />
+      <Icon name="rectangle" className={css.iconRectangle} />
     </div>
   );
 }
