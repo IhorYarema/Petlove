42d42a45e10ee25ac68e7bda046f50894de82bd5
Ihor <u96ok69@gmail.com>
2026-01-11 16:23:53 +0200

    fix petBlock, RegForm and RegPage

--- src/components/PetBlock/PetBlock.jsx
+++ src/components/PetBlock/PetBlock.jsx
@@ -13,6 +13,26 @@ export default function PetBlock() {
         className={css.img}
       />
       <Icon name="rectangle" className={css.iconRectangle} />
+
+      <div className={css.petCont}>
+        <div className={css.petAvatar}>
+          🐈
+          {/* <Icon name="cat" className={css.iconPet} /> */}
+        </div>
+        <div className={css.textCont}>
+          <div className={css.nameCont}>
+            <h3 className={css.petName}>Jack</h3>
+            <p className={css.birthday}>
+              <span>Birthday: </span>
+              18.10.2021
+            </p>
+          </div>
+          <p className={css.text}>
+            Jack is a gray Persian cat with green eyes. He loves to be pampered
+            and groomed, and enjoys playing with toys.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
