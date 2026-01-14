9f5d05991ec6113bc91265cab0733909b7d2b4f9
Ihor <u96ok69@gmail.com>
2026-01-14 18:47:06 +0200

    make NoticesItem for mobile

--- src/components/NoticesItem/NoticesItem.jsx
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -1,23 +1,47 @@
 import css from "./NoticesItem.module.css";
+import Icon from "../Icon/Icon";
 
 export default function NoticesItem({ item }) {
   return (
     <div className={css.item}>
       <img src={item.imgURL} alt="Notice's Image" className={css.img} />
+
+      <h3 className={css.title}>{item.title}</h3>
       <div className={css.infoCont}>
-        <h3 className={css.title}>{item.title}</h3>
         <p className={css.info}>
-          <span>Email: </span>
-          {item.name ? item.email : "no email"}
+          <span>Name </span>
+          {item.name}
         </p>
         <p className={css.info}>
-          <span>Address: </span>
-          {item.birthday ? item.address : "website only"}
+          <span>Birthday </span>
+          {item.birthday ? item.birthday : "Unknown"}
         </p>
         <p className={css.info}>
-          <span>Phone: </span>
-          {item.sex ? item.phone : "email only"}
+          <span>Sex </span>
+          {item.sex}
         </p>
+        <p className={css.info}>
+          <span>Species </span>
+          {item.species}
+        </p>
+        <p className={css.info}>
+          <span>Category </span>
+          {item.category}
+        </p>
+      </div>
+      <p className={css.comment}>{item.comment}</p>
+      <p className={css.price}>
+        {item.price ? "$" + item.price : "Price needs clarification"}
+      </p>
+      <div className={css.btnsCont}>
+        <button className={css.moreBtn}>Learn more</button>
+        <button className={css.favBtn}>
+          <Icon
+            className={`${css.iconHeart} ${css.iconHeart}`}
+            name="heart"
+            size={18}
+          />
+        </button>
       </div>
     </div>
   );
