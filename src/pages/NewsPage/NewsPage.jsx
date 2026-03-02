1353100b76641fa3b257c5d081b053b983ed51c5
Ihor <u96ok69@gmail.com>
2026-03-02 19:42:30 +0200

    make pagination for News

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,15 +1,24 @@
 import SearchField from "../../components/SearchField/SearchField";
 import Title from "../../components/Title/Title";
 import NewsList from "../../components/NewsList/NewsList";
+import Pagination from "../../components/Pagination/Pagination";
 import css from "./NewsPage.module.css";
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { fetchNews } from "../../redux/news/operations";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function NewsPage() {
   const [search, setSearch] = useState("");
   const [debouncedSearch, setDebouncedSearch] = useState("");
   const dispatch = useDispatch();
+
+  const page = useSelector((state) => state.news.page);
+  const totalPages = useSelector((state) => state.news.totalPages);
+  const perPage = useSelector((state) => state.news.perPage);
+
+  const handlePageChange = (newPage, limit) => {
+    dispatch(fetchNews({ page: newPage, limit }));
+  };
 
   useEffect(() => {
     const t = setTimeout(() => {
@@ -36,6 +45,13 @@ export default function NewsPage() {
           <SearchField value={search} onChange={setSearch} />
         </div>
         <NewsList className={css.list} />
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          perPage={perPage}
+          onPageChange={handlePageChange}
+          className={css.pagination}
+        />
       </div>
     </section>
   );
