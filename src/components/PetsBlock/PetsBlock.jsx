f0922b11b979338f765248315963f018c731aa10
Ihor <u96ok69@gmail.com>
2026-03-18 19:59:48 +0200

    fix css for PetsBlock and make AddPet

--- src/components/PetsBlock/PetsBlock.jsx
+++ src/components/PetsBlock/PetsBlock.jsx
@@ -1,9 +1,14 @@
+import AddPet from "../AddPet/AddPet";
 import PetsList from "../PetsList/PetsList";
 import css from "./PetsBlock.module.css";
 
 export default function PetsBlock() {
   return (
     <div className={css.petBlock}>
+      <div className={css.upperCont}>
+        <h3 className={css.title}>My pets</h3>
+        <AddPet />
+      </div>
       <PetsList />
     </div>
   );
