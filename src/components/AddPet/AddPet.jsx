68cf6a48a13b096ad374685c531b4092e2e4246c
Ihor <u96ok69@gmail.com>
2026-03-19 19:01:07 +0200

    make addPetBtn jsx and css

--- src/components/AddPet/AddPet.jsx
+++ src/components/AddPet/AddPet.jsx
@@ -1,5 +1,11 @@
 import css from "./AddPet.module.css";
+import Icon from "../Icon/Icon";
+import { NavLink } from "react-router-dom";
 
 export default function AddPet() {
-  return <button className={css.addBtn}>Add pet +</button>;
+  return (
+    <NavLink to="/add-pet" className={css.addBtn}>
+      Add pet <Icon className={css.iconUser} name="plus" size={18} />
+    </NavLink>
+  );
 }
