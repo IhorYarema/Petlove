6ebcca95a6d46b87e4a03a34a7520da8e6db111b
Ihor <u96ok69@gmail.com>
2026-03-20 19:59:08 +0200

    make css for AddPetForm

--- src/components/AddPetForm/AddPetForm.jsx
+++ src/components/AddPetForm/AddPetForm.jsx
@@ -4,21 +4,26 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { addPetSchema } from "../../schemas/addPetSchema";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPet } from "../../redux/auth/operations";
 import { toast } from "react-toastify";
+import { selectUserPets } from "../../redux/auth/selectors";
 
 export default function AddPetForm() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const pet = useSelector(selectUserPets);
 
   const {
     register,
+    watch,
     handleSubmit,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(addPetSchema),
   });
+
+  const avatarValue = watch("avatar");
 
   const onSubmit = async (data) => {
     try {
@@ -36,38 +41,79 @@ export default function AddPetForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {/* SEX (radio) */}
+      <button
+        className={`${css.sexBtn} ${css.male}`}
+        type="radio"
+        value="male"
+        {...register("sex")}
+      >
+        <Icon className={css.iconCloud} name="male" size={20} />
+      </button>
+
+      <button
+        className={`${css.sexBtn} ${css.female}`}
+        type="radio"
+        value="female"
+        {...register("sex")}
+      >
+        <Icon className={css.iconCloud} name="female" size={20} />
+      </button>
+
+      <button
+        className={`${css.sexBtn} ${css.unknown}`}
+        type="radio"
+        value="unknown"
+        {...register("sex")}
+      >
+        <Icon className={css.iconCloud} name="male-female" size={20} />
+      </button>
+
+      {/* ava */}
+      {!pet.avatar ? (
+        <div className={css.avatarEmpty}>
+          <Icon className={css.iconUser} name="footprint" size={34} />
+        </div>
+      ) : (
+        <img src={avatarValue || pet?.avatar} className={css.img} />
+      )}
+
       {/* IMG URL */}
-      <input {...register("imgUrl")} placeholder="Image URL" />
-      <p>{errors.imgUrl?.message}</p>
+      <div className={`${css.inputWrapper} ${css.firstInputWrapper}`}>
+        <input
+          className={css.input}
+          {...register("imgUrl")}
+          placeholder="Enter URL"
+        />
+        <div className={css.uploadContainer}>
+          <p>Upload photo</p>{" "}
+          <Icon className={css.iconCloud} name="upload-cloud" size={18} />
+        </div>
+      </div>
 
       {/* TITLE */}
       <input className={css.input} {...register("title")} placeholder="Title" />
-      <p>{errors.title?.message}</p>
 
       {/* NAME */}
-      <input {...register("name")} placeholder="Name" />
-      <p>{errors.name?.message}</p>
+      <input
+        className={css.input}
+        {...register("name")}
+        placeholder="Pet’s Name"
+      />
 
       {/* SPECIES */}
-      <input {...register("species")} placeholder="Species" />
-      <p>{errors.species?.message}</p>
+      <input
+        className={css.input}
+        {...register("species")}
+        placeholder="Species"
+      />
 
       {/* BIRTHDAY */}
-      <input {...register("birthday")} placeholder="YYYY-MM-DD" />
-      <p>{errors.birthday?.message}</p>
-
-      {/* SEX (radio) */}
-      <label>
-        <input type="radio" value="male" {...register("sex")} />
-        Male
-      </label>
-
-      <label>
-        <input type="radio" value="female" {...register("sex")} />
-        Female
-      </label>
-
-      <p>{errors.sex?.message}</p>
+      <input
+        className={css.input}
+        {...register("birthday")}
+        placeholder="00.00.0000"
+      />
 
       {/* BUTTONS */}
       <button type="submit">Submit</button>
